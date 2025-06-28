2e247ccfad214d51651f5b0a6a9c39e2486d7c41
Rajat Nanda <146905490+rajat-9090@users.noreply.github.com>
2025-06-28 07:58:35 +0530

    Update karate-config.js

--- HQM_Automation/HQM/src/test/java/karate-config.js
+++ HQM_Automation/HQM/src/test/java/karate-config.js
@@ -1,4 +1,4 @@
-function fn() {
+afunction fn() {
   var env = karate.env; // get system property 'karate.env'
   karate.log('karate.env system property was:', env);
   if (!env) {
@@ -9,9 +9,9 @@ function fn() {
     //Setting up the Driver to Run
     ChromeDriver: {
           type: 'chromedriver',
-          executable: 'C:\\Users\\Admin\\OneDrive - health-chain.io\\Desktop\\Extra_Applications\\All Drivers\\chromedriver.exe'
+          executable: 'C:\\Users\\Admin\\OneDrive - example.io\\Desktop\\Extra_Applications\\All Drivers\\chromedriver.exe'
         },
-    URL: 'https://hqm.health-chain.io'
+    URL: 'https://hqm.example.io'
   };
   if (env == 'dev') {
     // customize
